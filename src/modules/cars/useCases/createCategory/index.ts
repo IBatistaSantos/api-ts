0980e304adc50b8d4524ca5b53cab9fa775f2d2d
Israel Batista <israelbatiista19@gmail.com>
2021-03-21 11:32:08 -0300

    Developed create use case Categories

---
+++ src/modules/cars/useCases/createCategory/index.ts
@@ -0,0 +1,13 @@
+import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
+import { CreateCategoryController } from "./CreateCategoryController";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+
+const categoriesRepository = CategoriesRepository.getInstance();
+
+const createCategoriesUseCase = new CreateCategoryUseCase(categoriesRepository);
+
+const createCategoriesController = new CreateCategoryController(
+  createCategoriesUseCase
+);
+
+export { createCategoriesController };
