9d75a1ba725efb5328e680ad6018312917c98392
Israel Batista <israelbatiista19@gmail.com>
2021-04-16 10:45:49 -0300

    Developed integration test create category

--- src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
@@ -1,6 +1,7 @@
 import { injectable, inject } from "tsyringe";
 
-import { Category } from "../../entities/Category";
+import { Category } from "@modules/cars/infra/typeorm/entities/Category";
+
 import { ICategoryRepository } from "../../repositories/ICategoriesRepository";
 
 @injectable()
